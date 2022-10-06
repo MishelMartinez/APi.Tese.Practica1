3c52c86af98c594cc642de1b97b3bf6fd08b90f6
MishelMartinez <lilian.mishel06@gmail.com>
2022-10-06 13:34:59 -0500

    Creación de tabla de estudiantes y conexion a API

---
+++ Api.Tese.MVC/BusinessLogic/StudentBL.cs
@@ -0,0 +1,34 @@
+﻿using Api.Tese.Entities;
+
+namespace Api.Tese.MVC.BusinessLogic
+{
+    public class StudentBL
+    {
+        private static HttpClient client;
+
+        public StudentBL()
+        {
+            client = new HttpClient(); 
+        }
+
+
+        public async Task<List<Student>> GetStudents()
+        {
+            List < Student > students = new List<Student>();
+            using (client = new HttpClient())
+            {
+                var result = await client.GetFromJsonAsync<List<Student>>("https://localhost:44326/api/Student/GetStudents");
+
+                if(result!= null)
+                {
+                    students = result;
+                }
+
+               
+            }
+
+            return students;
+
+        }
+    }
+}
---
+++ Api.Tese.MVC/Controllers/StudentController.cs
@@ -0,0 +1,31 @@
+﻿using Api.Tese.Entities;
+using Api.Tese.MVC.BusinessLogic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Tese.MVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly StudentBL studentBL;
+
+
+        public StudentController()
+        {
+            studentBL = new StudentBL();
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            ViewBag.TestMessage = "A continuación se muestra el listado de los estudiantes en el curso";
+
+            ViewBag.listStudents = await studentBL.GetStudents();
+
+
+
+            return View();
+        }
+
+
+     
+    }
+}
